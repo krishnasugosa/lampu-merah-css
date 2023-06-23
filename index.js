243219fa07a0a6916be4c81db24b6796b7057893
code.shreek <127308616+krishnasugosa@users.noreply.github.com>
2023-06-23 19:59:18 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let div = document.querySelector('div')
+
+setInterval(function(){
+   div.classList.add("box1")
+},3000)
+
+setInterval(function(){
+   div.classList.add("box2")
+},6000)
+ 
+setInterval(function(){
+   div.classList.add("box3")
+},9000)
+
+console.warn("warning bro")
+
